c2facc1acad35e4f1ff934dbede28fee22872ebf
davidgress <david.e.gress@gmail.com>
2023-09-15 07:32:29 -0400

    Section 6: Begin adding SQLAlchemy

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
 
-stores = {
-
-}
+db = SQLAlchemy()
 
 
-items = {
-    # 1: {"name": "Chair", "price": 17.99},
-    # 2: {"name": "Table", "price": 180.50}
-}
+# stores = {
+#
+# }
+#
+#
+# items = {
+#     # 1: {"name": "Chair", "price": 17.99},
+#     # 2: {"name": "Table", "price": 180.50}
+# }

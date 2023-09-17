d4b48f2b4b6bb0026b4800e520ec259af7627688
davidgress <david.e.gress@gmail.com>
2023-09-17 07:18:22 -0400

    Section 6: Finish section 6. refactor post, put, delete.

--- resources/item.py
+++ resources/item.py
@@ -2,7 +2,9 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
@@ -12,65 +14,47 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted"}
-        except KeyError:
-            abort(404, message="item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        # item_data = request.get_json()
-        # if "price" not in item_data or "name" not in item_data:
-        #     abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.name = item_data["name"]
+            item.price = item_data["price"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
 
-        try:
-            item = items[item_id]
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        db.session.add(item)
+        db.session.commit()
+        return item
 
 
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
 
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
         """ item_data is JSON passed thru the ItemSchema """
-        # item_data = request.get_json()
-        if (
-                "price" not in item_data
-                or "store_id" not in item_data
-                or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
-        for item in items.values():
-            if (
-                    item_data["name"] == item["name"]
-                    and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
+        item = ItemModel(**item_data)
 
-        # if item_data["store_id"] not in stores:
-        #     abort(404, message="Store not found")
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            # db.session.rollback()
+            abort(500, message="An error occurred while inserting the item.")
 
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
         return item
